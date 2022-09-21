a0533bf20b172390545c26895d314f079c5b42c7
Pablo Santos <pablo.roldao.santos@gmail.com>
2022-09-21 20:39:45 -0300

    Corrigindo Login...

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,7 +3,7 @@ import React from "react";
 class Login extends React.Component {
     render() {
         return (
-            <form className="container w-75 rounded p-5" method="post" action="#home">
+            <form className="container w-75 rounded p-5" method="post" action="/submit-login">
                 <div className="text-center w-100">
                     <img src="./images/smt-logo.png" alt="Smart Meter Technology's Logo" className="rounded" />
                 </div>
@@ -15,7 +15,7 @@ class Login extends React.Component {
                     </div>
                 </div>
                 <div className="form-floating m-3">
-                    <input typef="password" className="form-control border border-dark" id="password" placeholder="Insira a senha..."></input>
+                    <input type="password" className="form-control border border-dark" id="password" placeholder="Insira a senha..."></input>
                     <label for="password">Senha</label>
                     <div>
                         Esqueceu a senha? <a href="/recover-password" className="link link-dark">Recuperar.</a>
