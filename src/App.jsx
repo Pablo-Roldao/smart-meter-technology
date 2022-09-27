a9200a4606c3b4561f56947f97ca614a10d68319
Pablo Santos <pablo.roldao.santos@gmail.com>
2022-09-27 17:14:37 -0300

    Ajustando Routes...

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
+import { Outlet } from "react-router-dom";
 
 class App extends React.Component {
 
   render() {
     return (
-      <>
-      </>
+      <div id="app-container">
+        <Outlet />
+      </div>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
+import App from "./App";
 import Home from "./pages/Home";
 import Login from './pages/Login';
 import RecoverPassword from './pages/RecoverPassword';
 import SignUp from './pages/SignUp';
 
-const router = createBrowserRouter([
+/*const router = createBrowserRouter([
   {
     path: "/",
     element: (
@@ -32,9 +33,19 @@ const router = createBrowserRouter([
       <SignUp />
     )
   },
-]);
+]);*/
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <RouterProvider router={router} />
-);
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route element={<App />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/recover-password" element={<RecoverPassword />} />
+          <Route path="/sign-up" element={<SignUp />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+)
