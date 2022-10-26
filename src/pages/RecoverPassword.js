a37d83f49ba3670fa0aed7bd64d4066d47197ed3
Pablo Santos <pablo.roldao.santos@gmail.com>
2022-10-26 09:36:07 -0300

    Arrumando forms para envio de dados...

--- src/pages/RecoverPassword.js
+++ src/pages/RecoverPassword.js
@@ -1,32 +1,56 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const RecoverPassword = () => {
-    return (
-        <form className="container w-75 rounded p-5" method="post" action="/recover-password-submit">
+class RecoverPassword extends React.Component {
 
-            <div className="text-center w-100">
-                <Link to="/">
-                    <img src="./images/smt-logo.png" alt="Smart Meter Technology's Logo" className="rounded w-25" />
-                </Link>
-            </div>
-            
-            <div className="form-floating m-3">
-                <input type="email" className="form-control border border-dark" id="password" placeholder="Insira a senha..."></input>
-                <label htmlFor="password">E-mail</label>
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: ""
+        };
 
-                <br />
+        this.setEmail = this.setEmail.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
 
-                <div>
-                    <Link to="/login" className="link link-dark">Entre</Link> ou <Link to="/sign-up" className="link link-dark">Cadastre-se.</Link>
+    setEmail(event) {
+        this.setState({
+            email: event.target.value
+        });
+    }
+
+    handleSubmit(event) {
+        alert("Recuperação de senha para o e-mail: " + this.state.email);
+        event.preventDefault();
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit} className="container w-75 rounded p-5">
+
+                <div className="text-center w-100">
+                    <Link to="/">
+                        <img src="./images/smt-logo.png" alt="Smart Meter Technology's Logo" className="rounded w-25" />
+                    </Link>
                 </div>
-            </div>
 
-            <div className="text-center">
-                <button type="submit" className="btn btn-outline-dark mb-3">Recuperar</button>
-            </div>
-        </form>
-    );
+                <div id="emailInput" className="form-floating m-3">
+                    <input id="email" name="email" type="email" value={this.state.email} onChange={(event) => this.setEmail(event)} required placeholder="Insira seu email..." className="form-control border border-dark" />
+                    <label htmlFor="email">E-mail</label>
+
+                    <br />
+
+                    <div>
+                        <Link to="/login" className="link link-dark">Entre</Link> ou <Link to="/sign-up" className="link link-dark">Cadastre-se.</Link>
+                    </div>
+                </div>
+
+                <div className="text-center">
+                    <button type="submit" className="btn btn-outline-dark mb-3">Recuperar</button>
+                </div>
+            </form >
+        );
+    }
 }
 
 export default RecoverPassword
--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -3,28 +3,78 @@ import { Link } from "react-router-dom";
 
 class SignUp extends React.Component {
 
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            name: "João",
+            email: "aa@aa",
+            password: "123",
+            passwordConfirmation: "123"
+        }
+
+        this.setName = this.setName.bind(this);
+        this.setEmail = this.setEmail.bind(this);
+        this.setPassword = this.setPassword.bind(this);
+        this.setPasswordConfirmation = this.setPasswordConfirmation.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    setName(event) {
+        this.setState({
+            name: event.target.value
+        });
+    }
+
+    setEmail(event) {
+        this.setState({
+            email: event.target.value
+        });
+    }
+
+    setPassword(event) {
+        this.setState({
+            password: event.target.value
+        });
+    }
+
+    setPasswordConfirmation(event) {
+        this.setState({
+            passwordConfirmation: event.target.value
+        });
+    }
+
+    handleSubmit(event) {
+        alert("Cadastro com:" + 
+        "\nName: " + this.state.name +
+        "\nE-mail: " + this.state.email +
+        "\nSenha: " + this.state.password +
+        "\nConfrimação de senha: " + this.state.passwordConfirmation);
+        event.preventDefault();
+    }
+
     render() {
         return (
-            <form className="container w-75 rounded p-5" method="post" action="/sign-up-submit">
+            <form onSubmit={this.handleSubmit} className="container w-75 rounded p-5">
                 <div className="text-center w-100">
                     <Link to="/">
                         <img src="./images/smt-logo.png" alt="Smart Meter Technology's Logo" className="rounded w-25" />
                     </Link>
                 </div>
-                <div className="form-floating m-3">
-                    <input type="text" className="form-control border border-dark shadow-sm" id="name" placeholder="Insira seu nome..." required></input>
+                <div id="nameInput" className="form-floating m-3">
+                    <input id="name" name="name" type="text" value={this.state.name} onChange={(event) => this.setName(event)} className="form-control border border-dark shadow-sm" placeholder="Insira seu nome..." required/>
                     <label for="name">Nome</label>
                 </div>
-                <div className="form-floating m-3">
-                    <input type="email" className="form-control border border-dark shadow-sm" id="email" placeholder="Insira seu e-mail..." required></input>
+                <div id="emailInput" className="form-floating m-3">
+                    <input id="email" name="email" type="email" value={this.state.email} onChange={(event) => this.setEmail(event)} className="form-control border border-dark shadow-sm" placeholder="Insira seu e-mail..." required/>
                     <label for="email">E-mail</label>
                 </div>
-                <div className="form-floating m-3">
-                    <input type="password" className="form-control border border-dark shadow-sm" id="password" placeholder="Insira a senha..." required></input>
+                <div id="passwordInput" className="form-floating m-3">
+                    <input id="password" name="password" type="password" value={this.state.password} onChange={(event) => this.setPassword(event)} className="form-control border border-dark shadow-sm" placeholder="Insira a senha..." required/>
                     <label for="password">Senha</label>
                 </div>
-                <div className="form-floating m-3">
-                    <input type="password" className="form-control border border-dark shadow-sm" id="passwordConfirmation" placeholder="Insira a senha novamente..." required></input>
+                <div id="passwordConfirmationInput" className="form-floating m-3">
+                    <input id="passwordConfirmation" name="passwordConfirmation" type="password" value={this.state.passwordConfirmation} onChange={(event) => this.setPasswordConfirmation(event)} className="form-control border border-dark shadow-sm" placeholder="Insira a senha novamente..." required/>
                     <label for="passwordConfirmation">Confirmar senha</label>
                     <br />
                     <div>
