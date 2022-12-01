828fb1e6dd7160aaa48d8b4a36a4004a230f0d6a
Pablo Santos <pablo.roldao.santos@gmail.com>
2022-11-30 21:39:29 -0300

    Linpando coisas desnecessárias e arrumando autenticação e colocando tradução...

--- src/App.js
+++ src/App.js
@@ -1,30 +1,138 @@
 import React from "react";
-import { Outlet } from "react-router-dom";
 
-import { Authenticator } from '@aws-amplify/ui-react';
-import '@aws-amplify/ui-react/styles.css';
+//import { Outlet } from "react-router-dom";
+
+import { Amplify, I18n } from "aws-amplify";
+
+import { Authenticator, View, useTheme, Image, Text, translations } from "@aws-amplify/ui-react";
+import "@aws-amplify/ui-react/styles.css";
+
+import awsExports from "./aws-exports";
+Amplify.configure(awsExports);
+
+I18n.putVocabularies(translations);
+I18n.setLanguage("pt");
+I18n.putVocabularies({
+  pt: {
+    "Create Account": "Criar conta",
+    "We Texted You": "Nós mandamos uma mensagem pra você",
+    "Your code is on the way. To log in, enter the code we sent you. It may take a minute to arrive.": "Seu código foi enviado para você. Para entrar, insira o código que foi enviado. Isso levará apenas um minuto e serve para a sua própria segurança.",
+    "Confirm": "Confirme",
+  }
+});
+
+I18n.putVocabulariesForLanguage('pt', {
+  "Reset Password": "Recuperar senha",
+  "Send code": "Enviar código",
+  "Back to Sign In": "Voltar para o login",
+  "confirme": "Confirme"
+});
+
+const components = {
+  Header() {
+    const { tokens } = useTheme();
+
+    return (
+      <View textAlign="center" padding={tokens.space.large}>
+        <Image
+          alt="SMT logo"
+          src="./images/smt-logo.png"
+          width={"50%"}
+        />
+      </View>
+    );
+  },
+  Footer() {
+    const { tokens } = useTheme();
+
+    return (
+      <View textAlign="center" padding={tokens.space.large}>
+        <Text color={tokens.colors.neutral[80]}>
+          &copy; Todos os direitos reservados para SMT
+        </Text>
+      </View>
+    );
+  },
+}
+const formFields = {
+  signIn: {
+    username: {
+      label: "E-mail",
+      placeholder: "Insira seu e-mail...",
+    },
+    password: {
+      label: "Senha",
+      placeholder: "Insira sua senha...",
+    }
+  },
+  signUp: {
+    name: {
+      order: 1,
+      label: "Nome",
+      placeholder: "Insira seu nome..."
+    },
+    email: {
+      order: 2,
+      label: "E-mail",
+      placeholder: "Insira seu e-mail...",
+    },
+    phone_number: {
+      dialCode: "+55",
+      order: 3,
+      label: "Celular",
+      placeholder: "Insira seu número de celular...",
+    },
+    password: {
+      order: 4,
+      label: "Senha",
+      placeholder: "Insira sua senha...",
+    },
+    confirm_password: {
+      order: 5,
+      label: "Confirmação de senha",
+      placeholder: "Insira sua senha novamente...",
+    },
+  },
+  resetPassword: {
+
+  },
+  confirmResetPassword: {
+    confirmation_code: {
+      placeholder: "Insira seu código de confirmação...",
+      label: "Código",
+      isRequired: false,
+    },
+    password: {
+      label: "Nova senha",
+      placeholder: "Insira sua nova senha...",
+    },
+    confirm_password: {
+      label: "Confirmação de senha",
+      placeholder: "Insira sua nova senha novamente...",
+    },
+  },
+}
 
 export default function App() {
   return (
     <>
-      <Authenticator signUpAttributes={[
-        'address',
-        'birthdate',
-        'email',
-        'name',
-        'phone_number'
-      ]}>
+      <Authenticator
+        components={components}
+        formFields={formFields}
+        signUpAttributes={["name", "phone_number"]}>
         {({ signOut, user }) => (
-          <main className="row">
-            <h3 className="col">Logado como {user.attributes.name}...</h3>
-            <button onClick={signOut} className="col btn btn-outline-dark">Sign out</button>
-            <div id="app-container">
-              <Outlet />
+          <main>
+            <div className="row p-3">
+              <h3 className="col">Logado como {user.attributes.name}...</h3>
+              <button onClick={signOut} className="col-2 btn btn-outline-dark">Sign out</button>
             </div>
+
+            {/*<div id="app-container">
+              <Outlet />
+            </div>*/}
           </main>
         )}
       </Authenticator>
-
     </>
   );
 }
--- src/index.jsx
+++ src/index.jsx
@@ -1,39 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import App from "./App";
-import Home from "./pages/Home";
-import Login from "./pages/Login";
-import RecoverPassword from "./pages/RecoverPassword";
-import SignUp from "./pages/SignUp";
 import Places from "./pages/Places";
-import FactoryMonitoring from "./pages/FactoryMonitoring";
-import NewMeasurement from "./pages/NewMeasurement";
-import Sensors from './pages/Sensors';
-
 import "./index.css";
-
-import { Amplify } from "aws-amplify";
-import awsExports from "./aws-exports";
-import SignUpConfirmation from './pages/SignUpConfirmation';
-Amplify.configure(awsExports);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
         <Route element={<App />}>
-          <Route path="/home" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/recover-password" element={<RecoverPassword />} />
-          <Route path="/sign-up" element={<SignUp />} />
-          <Route path="sign-up-confirmation" element={<SignUpConfirmation />}/>
           <Route path="/" element={<Places />} />
-          <Route path="/factory-monitoring" element={ <FactoryMonitoring />} />
-          <Route path="/new-measurement" element={<NewMeasurement />}/>
         </Route>
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
-)
+);
